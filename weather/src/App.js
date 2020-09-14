e49469ed3b9c53f461a07de7cc22b59443d191ed
macT <simmonst208@gmail.com>
2020-09-13 20:18:09 -0500

    file structure created and tools installed

---
+++ weather/src/App.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+import { Row, Col } from 'reactstrap';
+
+const App = () => {
+  return (
+    <Row>
+      <Col > <h1> Trev </h1></Col>
+  
+      <Col> <h3> Test </h3></Col>
+    </Row>
+
+
+
+
+  );
+}
+
+export default App;
